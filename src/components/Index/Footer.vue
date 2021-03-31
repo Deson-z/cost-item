7556bc6db7d77f6714c41ddb6f6ec8cd109deb75
Deson-Z <346412585@qq.com>
2021-04-01 00:02:47 +0800

    footer完成

--- src/components/Index/Footer.vue
+++ src/components/Index/Footer.vue
@@ -1,19 +1,58 @@
 <template>
   <div class="footer-wrap">
-    footer
+    <div class="content-wrap">
+      <img :src="brandImg" class="brand-img" alt="">
+      <ul class="f-text">
+        <li>联系我们</li>
+        <li>主办：深圳市造价工程师协会<i style="padding-left:40px">电话：（0755）83231498</i></li>
+        <li>地址：深圳市福田区汽车大厦四楼A409室</li>
+        <li>I粤ICP备14012334号-1</li>
+      </ul>
+      <img :src="QRcodeImg" class="qr-img" alt="">
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: "footer-wrap",
+  data(){
+    return {
+      brandImg:require('@/assets/img/f-logo.png'),
+      QRcodeImg:require('@/assets/img/qr.png')
+    }
+  }
 };
 </script>
 
 <style scoped>
+.f-text li{
+  padding-bottom: 22px;
+}
+.f-text{
+  padding-top: 14px;
+  font-size: 12px;
+  width: 444px;
+}
+.qr-img{
+  height: 158px;
+  width: 158px;
+}
+.brand-img{
+  width: 334px;
+  height: 82px;
+}
+.content-wrap{
+  padding: 48px 0 0 0;
+  margin: 0 auto;
+  width: 1360px;
+  font-size: 14px;
+  display: flex;
+  justify-content: space-between;
+}
 .footer-wrap{
     width: 100%;
-    height: 50px;
-    background: pink;
+    height: 252px;
+    background: #F1F1F1;
 }
 </style>
