f8834b2687588fa7c33e6274284dbafc88307b9e
ex_zhangds1 <ex_zhangds@partner.midea.com>
2021-03-31 18:21:10 +0800

    完成header

--- src/components/Index/Header.vue
+++ src/components/Index/Header.vue
@@ -1,19 +1,77 @@
 <template>
   <div class="header-wrap">
-    header
+    <div class="content-wrap">
+      <div class="l-item">{{currentDay}}</div>
+      <ul class="r-item">
+        <li class="r-item-li">
+          <i class="iconfont icon-dingdanchaxun"></i>
+          <span>信息价查询</span>
+        </li>
+        <li class="r-item-li">
+          <i class="iconfont icon-xunjia-"></i>
+          <span>询价</span>
+        </li>
+        <li class="r-item-li">
+          <i class="iconfont icon-denglu login"></i>
+          <span class="login">登录</span>
+          <span style="margin: 0 10px">|</span>
+          <span class="register">注册</span>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-    name:'header-wrap'
-}
+  name: "header-wrap",
+  data() {
+    return {
+      currentDay: "",
+    };
+  },
+  methods: {
+    getCurrentDay() {
+      this.currentDay = new Date().format("yyyy年MM月dd日 星期五 欢迎您");
+    },
+  },
+  mounted() {
+    this.getCurrentDay();
+  },
+};
 </script>
 
 <style scoped>
-.header-wrap{
-  height: 50px;
-  width: 100%;
+.register{
+  cursor:pointer;
 }
-
+.login {
+  cursor:pointer;
+  color: #fd000e;
+}
+.iconfont {
+  margin-right: 5px;
+}
+.r-item-li {
+  margin-right: 18px;
+}
+.r-item {
+  display: flex;
+  cursor:default;
+}
+.l-item {
+}
+.content-wrap {
+  margin: 0 auto;
+  line-height: 34px;
+  width: 1360px;
+  font-size: 14px;
+  display: flex;
+  justify-content: space-between;
+}
+.header-wrap {
+  height: 34px;
+  width: 100%;
+  background: #dededf;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -4,8 +4,10 @@ import router from './router'
 import store from './store'
 import './assets/style/common.css'
 import './assets/style/normalize.css'
+import './assets/fonts/iconfont.css'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
+import './lib/ulits'
 
 Vue.use(ElementUI)
 
